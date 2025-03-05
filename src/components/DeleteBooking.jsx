5b95b99931ab65c2903b2b13562f837f3362d0c1
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-05 20:12:04 +0100

    Implement Navbar and Loginpage

---
+++ src/components/DeleteBooking.jsx
@@ -0,0 +1,13 @@
+
+function DeleteBooking (){
+
+
+    return(
+        <>
+        <h1>Delete Booking</h1>
+        
+        </>
+    )
+}
+
+export default DeleteBooking;
---
+++ src/components/UpdateBooking.jsx
@@ -0,0 +1,12 @@
+function UpdateBooking (){
+
+
+    return(
+        <>
+        <h1>Update Booking</h1>
+        
+        </>
+    )
+}
+
+export default UpdateBooking;
