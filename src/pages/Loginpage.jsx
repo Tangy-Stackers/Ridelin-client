def16ec2c5e3275c0b6bae41b78c68cf61b9677c
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-10 09:53:03 +0100

    Update Loginpage

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -3,50 +3,76 @@ import { Link, useNavigate } from "react-router-dom";
 import { API_URL } from "../config/api";
 import { AuthContext } from "../context/auth.context";
 import axios from "axios";
+import { useForm } from "@mantine/form";
+import { Button, PasswordInput, TextInput } from "@mantine/core";
 
 
 
 function Loginpage() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState(undefined);
-    const navigate = useNavigate();
 
+
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState(undefined);
     const { storeToken, authenticateUser } = useContext(AuthContext);
+
     const handleEmail = (e) => setEmail(e.target.value);
     const handlePassword = (e) => setPassword(e.target.value);
 
-    const handleLoginSubmit = (e) => {
-        e.preventDefault();
-        const requestBody = { email, password };
-        console.log(email);
-        console.log(password);
-        axios.post(`${API_URL}/auth/login`, requestBody)
-            .then((response) => {
-                console.log('JWT token', response.data.authToken);
-                storeToken(response.data.authToken);
-                console.log("helloooo");
-                authenticateUser();
-                navigate('/');
-            })
-            .catch((error) => {
-                let errorDescription = "Something went wrong. Please try again.";
-                console.log(error);
-                setErrorMessage(errorDescription);
-            })
-    };
+    const form = useForm({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validate: {
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+            password: (value) => (value.length >= 6 ? null : "Password must be at least 6 characters"),
+        },
+    });
+      const handleLoginSubmit = (e) => {
+           e.preventDefault();
+           const { email, password } = form.values;
+           const requestBody = { email, password };
+           console.log(email);
+           console.log(password);
+           axios.post(`${API_URL}/auth/login`, requestBody)
+               .then((response) => {
+                   console.log('JWT token', response.data.authToken);
+                   storeToken(response.data.authToken);
+                   authenticateUser();
+                   navigate('/');
+               })
+               .catch((error) => {
+                   let errorDescription = "Something went wrong. Please try again.";
+                   console.log(error);
+                   setErrorMessage(errorDescription);
+               })
+       };
+
+
     return (
         <>
             <div className="LoginPage">
                 <h1>Login</h1>
 
                 <form onSubmit={handleLoginSubmit}>
-                    <label>Email:</label>
-                    <input type="email" name="email" value={email} onChange={handleEmail} />
-                    <label>Password:</label>
-                    <input type="password" name="password" value={password} onChange={handlePassword} />
-                    <button type="submit">Login</button>
-                </form>
+                    <TextInput
+                        withAsterisk
+                        label="Email"
+                        placeholder="your@email.com"
+                        key={form.key('email')}
+                        {...form.getInputProps('email')}
+                        required
+                    />
+                    <PasswordInput
+                        withAsterisk
+                        label="Password"
+                        placeholder="••••••••"
+                        {...form.getInputProps("password")}
+                        required
+                        mt="md"
+                    />
+                     <Button type="submit" fullWidth color="blue" mt="lg">Login</Button>
+            </form>
                 {errorMessage && <p className="error-message">{errorMessage}</p>}
                 <p>Don't have an account yet?</p>
                 
