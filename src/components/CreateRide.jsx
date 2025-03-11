455690d0c72f3d1b2372b6adf326c7ad57e845a4
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-03-11 21:06:00 +0100

    Add additional information to create ride.

--- src/components/CreateRide.jsx
+++ src/components/CreateRide.jsx
@@ -2,101 +2,175 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../config/api";
-
+import { Alert, Button, TextInput, Popover } from "@mantine/core";
+import { DatePicker, TimeInput } from '@mantine/dates';
+import { dateFormatter } from "../utils/dateFormatter";
 
 function CreateRide() {
     const userId = localStorage.getItem('userId')
     const storedToken = localStorage.getItem("authToken");
-    
-    const navigate = useNavigate()
+    const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const navigate = useNavigate();
+
     const [CreateRideData, SetCreateRideData] = useState({
         origin: " ",
         destination: " ",
-        seatsAvailable:" ",
+        seatsAvailable: " ",
         price: " ",
+        travelDate: "",
+        startTime: "",
+        endTime: "",
+        distance: "",
         driverId: userId,//userid
-      });
+    });
 
-    
+
+
+
     const handleChange = (e) => {
 
         SetCreateRideData({
-          ...CreateRideData, 
-        //   driverId: UserId, add userid to become the driver id
-          [e.target.name]: e.target.value,
+            ...CreateRideData,
+            //   driverId: UserId, add userid to become the driver id
+            [e.target.name]: e.target.value,
         });
-      };
+    };
 
-   
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
         //Creating Ride on API
-       
-            axios.post(`${API_URL}/api/ride`, CreateRideData,
-                { headers: { Authorization: `Bearer ${storedToken}`} }
-            )
-            
-            .then((response) =>{
-                console.log('Succeful ride created',response.data)
-                const newRideId = response.data._id; 
-                navigate(`/ride/${newRideId}`)})
 
-            .catch(e => console.log(" Error creating the new ride ...", e) )
+        axios.post(`${API_URL}/api/ride`, CreateRideData,
+            { headers: { Authorization: `Bearer ${storedToken}` } }
+        )
 
-            console.log("Form Submitted:", CreateRideData);
+            .then((response) => {
+                console.log('Succeful ride created', response.data)
+                const newRideId = response.data._id;
+                navigate(`/ride/${newRideId}`)
+            })
+
+            .catch(e => console.log(" Error creating the new ride ...", e))
+
+        console.log("Form Submitted:", CreateRideData);
     };
-   
+
 
     return (
         <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4 border rounded shadow">
             <h1>Create a Ride</h1>
+
             <div className="mb-4">
-            <label className="cs">Start location:</label>
-            <input
-                type="text"
-                name="origin"
-                value={CreateRideData.origin}
-                onChange={handleChange}
-                className="w-full border p-2 rounded"
-                required
-            />
+                <label className="block text-sm font-medium">Start location:</label>
+                <input
+                    type="text"
+                    name="origin"
+                    value={CreateRideData.origin}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
             </div>
+
             <div className="mb-4">
-            <label className="block text-sm font-medium">End location:</label>
-            <input
-                type="text"
-                name="destination"
-                value={CreateRideData.destination}
-                onChange={handleChange}
-                className="w-full border p-2 rounded"
-                required
-            />
+                <label className="block text-sm font-medium">End location:</label>
+                <input
+                    type="text"
+                    name="destination"
+                    value={CreateRideData.destination}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
             </div>
+
             <div className="mb-4">
-            <label className="block text-sm font-medium">Seats Available:</label>
-            <input
-                type="number"
-                name="seatsAvailable"
-                value={CreateRideData.seatsAvailable}
-                onChange={handleChange}
-                className="w-full border p-2 rounded"
-                required
-            />
+                <label className="block text-sm font-medium">Travel Date:</label>
+                <Popover opened={isDatePickerOpen} onClose={() => setIsDatePickerOpen(false)} position="bottom" withArrow>
+                    <Popover.Target>
+                        <TextInput
+                            value={selectedDate ? dateFormatter(selectedDate) : ""}
+                            placeholder="Select a Date"
+                            onClick={() => setIsDatePickerOpen(true)}
+                            readOnly
+                        />
+                    </Popover.Target>
+                    <Popover.Dropdown>
+                        <DatePicker
+                            value={selectedDate}
+                            onChange={(date) => {
+                                setSelectedDate(date);
+                                setIsDatePickerOpen(false);
+                            }}
+                        />
+                    </Popover.Dropdown>
+                </Popover>
             </div>
+
             <div className="mb-4">
-            <label className="block text-sm font-medium">Price(per person):</label>
-            <input
-                type="text"
-                name="price"
-                value={CreateRideData.price}
-                onChange={handleChange}
-                className="w-full border p-2 rounded"
-                required
-            />
+                <label className="block text-sm font-medium">Start Time:</label>
+                <TimeInput
+                    name="startTime"
+                    value={CreateRideData.startTime}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
             </div>
+
+            <div className="mb-4">
+                <label className="block text-sm font-medium">Approximate End Time:</label>
+                <TimeInput
+                    name="endTime"
+                    value={CreateRideData.endTime}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+            </div>
+
+            <div className="mb-4">
+                <label className="block text-sm font-medium">Approximate Distance:</label>
+                <input
+                    type="text"
+                    name="distance"
+                    value={CreateRideData.distance}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+            </div>
+
+            <div className="mb-4">
+                <label className="block text-sm font-medium">Seats Available:</label>
+                <input
+                    type="number"
+                    name="seatsAvailable"
+                    value={CreateRideData.seatsAvailable}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+            </div>
+
+            <div className="mb-4">
+                <label className="block text-sm font-medium">Price (per person):</label>
+                <input
+                    type="text"
+                    name="price"
+                    value={CreateRideData.price}
+                    onChange={handleChange}
+                    className="w-full border p-2 rounded"
+                    required
+                />
+            </div>
+
             <button type="submit" className="bg-blue-500 text-white p-2 rounded">Submit</button>
-      </form>
+        </form>
     );
 }
 
--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,7 +1,8 @@
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { API_URL } from "../config/api";
+import { Button } from "@mantine/core";
+import { Link, useNavigate } from "react-router-dom";
 
 
 
@@ -9,6 +10,7 @@ const UserProfile = () => {
     const [userId, setUserId] = useState(localStorage.getItem('userId'));
     const [storedToken, setStoredToken] = useState(localStorage.getItem("authToken"));
     const [profileDetails, setProfileDetails] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (!userId || !storedToken) {
@@ -19,25 +21,35 @@ const UserProfile = () => {
         axios.get(`${API_URL}/api/user/${userId}`, {
             headers: { Authorization: `Bearer ${storedToken}` }
         })
-        .then((response) => {
-            console.log(response.data);
-            setProfileDetails(response.data);
-        })
-        .catch(e => {
-            console.log('Error getting the user data', e);
-        });
+            .then((response) => {
+                console.log(response.data);
+                setProfileDetails(response.data);
+            })
+            .catch(e => {
+                console.log('Error getting the user data', e);
+            });
     }, [userId, storedToken]); // Ensure `userId` is included
+
+    const handleUpdate = () => {
+        navigate(`/user/${userId}/edit`);
+    }
 
     return (
         <div>
             {profileDetails ? (
-                <div>
+                <div className="UserDetails">
+                 <img className="img" src={profileDetails.image} alt="User Profile" />
                     <h1>{profileDetails.name}</h1>
-                    <p>Email: {profileDetails.email}</p>
+                    <p>✉️<b>Email:</b> {profileDetails.email}</p>
+                    <p>📞<b>Phone Number:</b>{profileDetails.phone}</p>
+                    <p>💭 <b>About me:</b> {profileDetails.about}</p>
+                    <Button onClick={handleUpdate}>Edit profile</Button>
                 </div>
             ) : (
                 <p>Loading...</p>
             )}
+            
+
         </div>
     );
 };
