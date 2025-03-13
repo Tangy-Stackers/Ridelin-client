841c0c870450866e05c936cafbebe9265454c9f1
OlgaML <omolina@geekcowsd.com>
2025-03-13 13:32:16 +0100

    add homepage css

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -12,61 +12,78 @@ function HomePage() {
   if (user) {
     navigate("/dashboard")
   }
-    
 
-    return (
-      <Container size="xl" py={40} >
-        <Center>
-          <Title order={1}>
+
+  return (
+    <Container size="xl" py={40} >
+      <Center>
+        <Title order={4} align="center">
+          Share a Ride, Save Money, Meet New People, and Make a Difference
+        </Title>
+      </Center>
+      <br />
+      <Center>
+        <Title order={1} size={90} c={"#FF5733"} shadow="sm">
+          <strong>
             Welcome to Ridelin
-          </Title>
-        </Center>
-        <Center>
-          <Title order={2} align="center" weight={500}>
-            The Future of Commuting: Share a Ride, Save Money, Meet New People, and Make a Difference
-          </Title>
-        </Center>
+          </strong>
+        </Title>
+      </Center>
+      <Center>
+        <Title order={3} align="center" weight={500} c={"#0D0402"}>
+          The Future of Commuting
+        </Title>
+      </Center>
 
-        <Center mt={20}>
-          <SearchRide />
-        </Center>
 
-        <Stack spacing="xl" mt={40} align="center">
-          <Title order={2}>Tired of commuting alone?</Title>
+      <Center mt={20}>
+        <SearchRide />
+      </Center>
+
+      <Stack spacing="xl" mt={40} align="center">
+        <Title order={1}>Tired of commuting alone?</Title>
+        <Container>
           <Text weight={700} align="center" size="lg">
             Meet Ridelin, the carpooling app that makes sharing rides easy, affordable, and eco-friendly.
           </Text>
-          <Text weight={700} align="center" size="lg">
-            Whether you're commuting to work, running errands, or embarking on a road trip, join trusted riders and drivers to share costs, cut emissions, and make every ride more enjoyable!
-          </Text>
-        </Stack>
+        </Container>
 
-        <Title order={2} mt={50} align="center">
-          Why us?
-        </Title>
-        <Group spacing="lg" mt={27} mb={27} align="center" position="center" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+        <Container px={50} size="sm">
+          <Text weight={700} align="center" size="lg">
+            Whether you're commuting to work, running errands, or embarking on a road trip
+            join trusted riders and drivers to share costs, cut emissions, and make every ride more enjoyable!
+          </Text>
+
+        </Container>
+
+      </Stack>
+
+      <Title order={2} mt={50} align="center">
+        Why us?
+      </Title>
+      <Group spacing="lg" mt={27} mb={27} align="center" position="center" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
         <Card shadow="sm" padding="lg" radius="md" withBorder>
-         
-          <Text  size="lg">💰 <strong>Save Money</strong></Text>
-         
+
+          <Text size="lg">💰 <strong>Save Money</strong></Text>
+
           <Text>Share rides and cut down on fuel expenses.</Text>
         </Card>
 
         <Card shadow="sm" padding="lg" radius="md" withBorder>
-        
+
           <Text size="lg">🌱<strong>Drive Less, Breathe More</strong> </Text>
-         
+
           <Text>Reduce traffic and CO₂, shrinking your carbon footprint.</Text>
         </Card>
 
         <Card shadow="sm" padding="lg" radius="md" withBorder>
-          
+
           <Text size="lg">🤝 <strong>Meet New People</strong></Text>
-          
+
           <Text>Connect with fellow travelers and build community.</Text>
         </Card>
       </Group>
-      </Container>
-    );
-  }
+    </Container>
+  );
+}
 export default HomePage;
